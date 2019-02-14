25ae8dbf0fae360b0ef371ad40ac8ed59473eaa9
Erik Behnke <erik.behnke@gmail.com>
2019-02-14 16:27:41 -0600

    add first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -1,4 +1,12 @@
 // helloWorld function 
-function helloWorld() {
-    return "Hello, World!";
+// function helloWorld() {
+//     return "Hello, World!";
+// }
+
+var helloWorld = function() {
+    return "Hello, World!"
+}
+
+function sayHello() {
+    return "Hello";
 }
